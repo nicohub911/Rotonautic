689cecfe3555fee0bd1e8f05c06acb5386dcb692
nicolas <nicolaser911@gmail.com>
2025-07-02 21:07:42 -0300

    fixed some design errors and i made the design of the kayaks pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,17 @@
+//nav
 const btnDropdownNav = document.getElementById("btnDropdownNav");
 const navOptions = document.getElementById("navOptions");
-/*  calc(100vh - 40px)  */
+//products sliders
+const gallerySlider = document.getElementById("gallerySlider");
+const product_leftBtn = document.getElementById("product_leftBtn");
+const product_rightBtn = document.getElementById("product_rightBtn");
+let actualSlide = 0;
+// depend of the situation modified the type of nav
 if (window.innerWidth < 600) {
     navOptions.style.height = "0px";
-    navOptions.style.opacity = "0";    
+    navOptions.style.opacity = "0";
 }
-window.addEventListener("resize", ()=>{
+window.addEventListener("resize", () => {
     if (window.innerWidth < 600) {
         navOptions.style.height = "0px";
         navOptions.style.opacity = "0";
@@ -14,12 +20,31 @@ window.addEventListener("resize", ()=>{
         navOptions.style.opacity = "1";
     }
 });
-btnDropdownNav.addEventListener("click", ()=>{
+btnDropdownNav.addEventListener("click", () => {
     if (navOptions.style.height == "0px") {
         navOptions.style.height = "calc(100vh - 40px)";
-        navOptions.style.opacity = "1"; 
+        navOptions.style.opacity = "1";
     } else {
         navOptions.style.height = "0px";
-        navOptions.style.opacity = "0"; 
+        navOptions.style.opacity = "0";
     }
+});
+// function of the gallery kayaks slider (manage the slider movement)
+function movementSlider(nSlides, direction) {
+    let slideStep = 100 / nSlides;
+
+    if (direction === "L" && actualSlide !== 0) {
+        actualSlide = actualSlide + slideStep;
+        gallerySlider.style.transform = `translateX(${actualSlide}%)`;
+    } else if (direction === "R" && actualSlide !== (-100 + slideStep)) {
+        actualSlide = actualSlide - slideStep;
+        gallerySlider.style.transform = `translateX(${actualSlide}%)`;
+    }
+}
+// listeners for the buttons of gallery kayaks.
+product_leftBtn.addEventListener("click", () => {
+    movementSlider(parseInt(product_leftBtn.dataset.kayak), "L");
+});
+product_rightBtn.addEventListener("click", () => {
+    movementSlider(parseInt(product_rightBtn.dataset.kayak), "R");
 });
