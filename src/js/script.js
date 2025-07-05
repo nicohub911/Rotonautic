fdbc84c473c5e492ae814e5be90847dd9e4033d3
nicolas <nicolaser911@gmail.com>
2025-07-05 18:40:37 -0300

    i made changes in the banner of <<inicio>> and litle more things

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,50 @@
 //nav
 const btnDropdownNav = document.getElementById("btnDropdownNav");
 const navOptions = document.getElementById("navOptions");
+//home (index) slider
+const homeSlider = document.getElementById("homeSlider");
+let homeActualSlide = 0;
 //products sliders
 const gallerySlider = document.getElementById("gallerySlider");
 const product_leftBtn = document.getElementById("product_leftBtn");
 const product_rightBtn = document.getElementById("product_rightBtn");
 let actualSlide = 0;
+//
+if (homeSlider) {
+    setInterval(() => {
+        switch (homeActualSlide) {
+            case 0:
+                homeSlider.style.transition = "0.3s";
+                homeActualSlide = homeActualSlide + 1;
+                break;
+            case 1:
+                homeSlider.style.transform = "translateX(-20%)";
+                homeActualSlide = homeActualSlide + 1;
+                break;
+            case 2:
+                homeSlider.style.transform = "translateX(-40%)";
+                homeActualSlide = homeActualSlide + 1;
+                break;
+            case 3:
+                homeSlider.style.transform = "translateX(-60%)";
+                
+                homeActualSlide = homeActualSlide + 1;
+                break;
+            case 4:
+                homeSlider.style.transform = "translateX(-80%)";
+                homeActualSlide = 0;
+
+                let resetSlider = setTimeout(() => {
+                    homeSlider.style.transition  = "none";
+                    homeSlider.style.transform = "translateX(0%)";
+                }, 300);
+                break;
+            default:
+                console.error("Error with the home slider index");
+                break;
+        }
+    }, 4000);
+}
 // depend of the situation modified the type of nav
 if (window.innerWidth < 600) {
     navOptions.style.height = "0px";
@@ -30,7 +69,7 @@ btnDropdownNav.addEventListener("click", () => {
     }
 });
 // function of the gallery kayaks slider (manage the slider movement)
-function movementSlider(nSlides, direction) {
+function productsMovementSlider(nSlides, direction) {
     let slideStep = 100 / nSlides;
 
     if (direction === "L" && actualSlide !== 0) {
@@ -42,9 +81,11 @@ function movementSlider(nSlides, direction) {
     }
 }
 // listeners for the buttons of gallery kayaks.
-product_leftBtn.addEventListener("click", () => {
-    movementSlider(parseInt(product_leftBtn.dataset.kayak), "L");
-});
-product_rightBtn.addEventListener("click", () => {
-    movementSlider(parseInt(product_rightBtn.dataset.kayak), "R");
-});
+if (product_leftBtn && product_rightBtn) {
+    product_leftBtn.addEventListener("click", () => {
+        productsMovementSlider(parseInt(product_leftBtn.dataset.kayak), "L");
+    });
+    product_rightBtn.addEventListener("click", () => {
+        productsMovementSlider(parseInt(product_rightBtn.dataset.kayak), "R");
+    });   
+}
